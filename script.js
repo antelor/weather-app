b984af2749ee57a3c02c441eebfe90d524dc5f55
antelor <ramiro.antelo4@gmail.com>
2021-07-19 14:31:56 -0300

    css basico agregado

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ async function weatherCheck(city) {
     const apiKey = '7e15537baea7ddec4fb11ef35343500b';
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`, {mode: 'cors'});
     const weatherData = await response.json();
-    //console.log(weatherData);
+    console.log(weatherData);
 
     let dataArr = {
         'city': city,
         'temp': weatherData.main.temp,
+        'temp_max': weatherData.main.temp_max,
+        'temp_min': weatherData.main.temp_min,
     };
 
     weatherDisplay(dataArr);
 }
 
 let weatherDisplay = (arr) => {
-    let div = document.querySelector('[class=weatherDiv]');
+    let div = document.querySelector('[class=mainWeather]');
 
     let cityDiv = document.querySelector('[class=cityDiv]');
     if ( cityDiv == null ) {
@@ -21,7 +23,7 @@ let weatherDisplay = (arr) => {
         cityDiv.classList.add('cityDiv');
     }
     cityDiv.innerText = arr.city;
-
+    
     let tempDiv = document.querySelector('[class=tempDiv]');
     if ( tempDiv == null ) {
         tempDiv = document.createElement('div');
@@ -29,8 +31,16 @@ let weatherDisplay = (arr) => {
     }
     tempDiv.innerText = arr.temp;
 
+    let minMax = document.querySelector('[class=minMaxDiv]');
+    if ( minMax == null ) {
+        minMax = document.createElement('div');
+        minMax.classList.add('minMaxDiv');
+    }
+    minMax.innerText = `Max: ${arr.temp_max} Min: ${arr.temp_min}`;
+
     div.appendChild(cityDiv);
     div.appendChild(tempDiv);
+    div.appendChild(minMax);
 }
 
 document.querySelector('[class=btnSearch').addEventListener("click", () => {
